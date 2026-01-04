1eb0e75f95f8ead6f736ba71297214a30e97b376
Zachary Lam <zlam@rrc.ca>
2026-01-04 13:36:12 -0600

    Created computed property for temperatureOutput and a Section with a Text View to show the converted output of the temperature

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -12,6 +12,36 @@ struct ContentView: View {
     @State private var outputUnit: String = "C"
     @State private var temperatureInput: Double = 0.0
     
+    var temperatureOutput: Double {
+        if inputUnit == "F" {
+            if outputUnit == "C" {
+                return (temperatureInput - 32) * 5 / 9
+            } else if outputUnit == "K" {
+                return (temperatureInput - 32) * 5 / 9 + 273.15
+            } else {
+                return temperatureInput
+            }
+        } else if inputUnit == "C" {
+            if outputUnit == "F" {
+                return (temperatureInput * 9 / 5) + 32
+            } else if outputUnit == "K" {
+                return temperatureInput + 273.15
+            } else {
+                return temperatureInput
+            }
+        } else if inputUnit == "K" {
+            if outputUnit == "F" {
+                return (temperatureInput - 273.15) * 9 / 5 + 32
+            } else if outputUnit == "C" {
+                return temperatureInput - 273.15
+            } else {
+                return temperatureInput
+            }
+        } else {
+            return temperatureInput
+        }
+    }
+    
     let units: Array<String> = ["F", "C", "K"]
     
     var body: some View {
@@ -38,6 +68,10 @@ struct ContentView: View {
                 Section("Temperature (\(inputUnit)°)") {
                     TextField("Temperature Input", value: $temperatureInput, format: .number)
                         .keyboardType(.decimalPad)
+                }
+                
+                Section("Temperature (\(outputUnit)°)") {
+                    Text(temperatureOutput, format: .number.precision(.fractionLength(1)))
                 }
             }
             .navigationTitle("Unit Converter")
